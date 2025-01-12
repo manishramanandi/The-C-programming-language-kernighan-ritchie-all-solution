b4d1eb5dc303ebb7b666cebffd78fb292a130ed1
manishramanandi <bairagimanish222@gmail.com>
2025-01-12 13:38:39 +0530

    Exercise1-1

---
+++ Exercise1-1.c
@@ -0,0 +1,15 @@
+//Run the "hello, world" program on your system. 
+//Experiment with leaving out parts of the program, 
+//to see what error messages you get.
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello world");
+    return 0;
+}
+
+// I am just not writing \n to see what error the compiler 
+// will give. Try some experiment by yourself  
+
+
